a98ea075e4e1c621fd4e8af0450d6c795151e05c
NiveSJ <niveathithavj@gmail.com>
2023-01-23 12:53:53 +0100

    Initial commit

---
+++ src/test/java/se/lexicon/springboot_workshop/SpringBootWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.springboot_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
